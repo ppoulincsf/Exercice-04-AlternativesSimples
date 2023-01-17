fe0fcaadca4f45b06fd16eb816db53cec9e53b22
Pierre Poulin <ppoulin@cegep-ste-foy.qc.ca>
2023-01-17 11:03:58 -0500

    code ETU

---
+++ exercice-05/Exercice05/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace Exercice05
+{
+    public class Program
+    {
+        //Constantes pour le numéro 1.3
+        public const float HIGH_COMMISSION_MINIMAL_SALES = 10_000.00f;
+        public const float HIGH_COMMISSION_RATE = 0.10f;
+        public const float LOW_COMMISSION_RATE = 0.085f;
+
+        //Constantes pour le numéro 1.4
+        public const int EXAM_1_VALUE = 60;
+        public const int EXAM_2_VALUE = 40;
+        public const int MINIMAL_GRADE = 60;
+
+        //Constantes pour le numéro 1.5
+        public const float COST_PER_COURSE_RESIDENT = 150.00f;
+        public const float COST_PER_COURSE_NON_RESIDENT = 320.00f;
+
+        //Constantes pour le numéro 1.6
+        public const float COST_ACCESS = 4.00f;
+        public const float COST_SKATING = 4.00f;
+        public const float COST_SKIING = 6.00f;
+        public const float CHILD_REBATE_RATE = 0.50f;
+        public const int CHILD_REBATE_MAX_AGE = 12;
+
+        public static void Main(string[] args)
+        {
+            FindSmallerNumber2Values();
+            FindSmallerNumber3Values();
+            ComputeSellerCommission();
+            HasStudentPassed();
+            ComputeTuitionFees();
+            ComputeTicketPrice();
+        }
+
+        public static void FindSmallerNumber2Values()
+        {
+            //Entrées
+
+
+            // Algorithme
+
+
+            // Sortie
+            
+        }
+
+        public static void FindSmallerNumber3Values()
+        {
+            //Entrées
+
+
+            // Algorithme
+
+
+            // Sortie
+            
+        }
+
+        public static void ComputeSellerCommission()
+        {
+            //Entrées
+
+
+            // Algorithme
+
+
+            // Sortie
+            
+        }
+
+        public static void HasStudentPassed()
+        {
+            //Entrées
+
+
+            // Algorithme
+
+
+            // Sortie
+            
+        }
+
+        public static void ComputeTuitionFees()
+        {
+            //Entrées
+
+
+            // Algorithme
+
+
+            // Sortie
+            
+        }
+
+        public static void ComputeTicketPrice()
+        {
+            //Entrées
+
+
+            // Algorithme
+
+
+            // Sortie
+            
+        }
+    }
+}
